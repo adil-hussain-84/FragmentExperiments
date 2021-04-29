feb7e135d463bfb63f3e8cf65fccb0e3336723cb
Adil Hussain <adilson05uk@gmail.com>
2021-04-29 14:12:36 +0100

    (app1) Removed `ParentFragment` since the problem can be reproduced without it.

--- app1/src/main/java/com/tazkiyatech/app1/MainActivity.kt
+++ app1/src/main/java/com/tazkiyatech/app1/MainActivity.kt
@@ -2,24 +2,63 @@ package com.tazkiyatech.app1
 
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var _fragmentContainer: ViewGroup? = null
+
+    private val fragmentContainer: ViewGroup
+        get() {
+            var frameLayout = _fragmentContainer
+            if (frameLayout == null) {
+                frameLayout = FrameLayout(this)
+                frameLayout.id = R.id.fragmentContainer
+                frameLayout.layoutParams = FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.MATCH_PARENT
+                )
+                _fragmentContainer = frameLayout
+            }
+            return frameLayout
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        val fragment = supportFragmentManager.findFragmentById(R.id.rootView)
+        val contentView = FrameLayout(this)
+        contentView.id = R.id.contentView
+        contentView.layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT
+        )
+
+        val customFrameLayout = findViewById<FrameLayout>(R.id.rootView)
+        customFrameLayout.addView(contentView)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val frameLayout = findViewById<FrameLayout>(R.id.rootView)
+        val contentView = frameLayout.getChildAt(0) as ViewGroup
+
+        contentView.removeAllViews()
+        contentView.addView(fragmentContainer)
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
 
         if (fragment == null) {
-            Log.d("Foo", "Null fragment in rootView")
+            Log.d("Foo", "Null fragment in fragmentContainer")
+
             supportFragmentManager.beginTransaction()
-                .replace(R.id.rootView, ParentFragment())
+                .replace(R.id.fragmentContainer, ChildFragment())
                 .commit()
         } else {
-            Log.d("Foo", "Non-null fragment in rootView")
+            Log.d("Foo", "Non-null fragment in fragmentContainer")
         }
     }
 }
